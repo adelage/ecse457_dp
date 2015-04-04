a6cfb5b9191853704988669c5ca1c229a5b6b67d
Anthony Delage <anthony.delage@gmail.com>
2015-04-04 16:01:11 -0400

    Fixed typo in FuelSensor.c

--- critical_library/FuelSensor.c
+++ critical_library/FuelSensor.c
@@ -24,7 +24,7 @@ RT_MODEL_FuelSensor_T *const FuelSensor_M = &FuelSensor_M_;
 
 /* Model step function */
 void FuelSensor_step(boolean_T* fs_warning_light, real32_T* fs_fuel_level)
-{*
+{
   /* UnitDelay: '<Root>/Light on  ' */
   *fs_warning_light = FuelSensor_DW.Lighton_DSTATE;
 
