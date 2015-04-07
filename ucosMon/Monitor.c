490ffd29e8687df6bb2946553734f1a84b2eb6a3
Anthony Delage <anthony.delage@gmail.com>
2015-04-07 01:36:45 -0400

    Added OSTimeGet to Monitor.c.

--- ucosMon/Monitor.c
+++ ucosMon/Monitor.c
@@ -124,6 +124,7 @@ void schedule_task(void* pdata){
 
 		// Run Sum function
 		sum_task(&s_args);
+		x = OSTimeGet();
 
 		// //Acquire the mutex and set cores 1 and 2 to execute the first task
 		// altera_avalon_mutex_lock(mutex, 1);
