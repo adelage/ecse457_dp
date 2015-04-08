9db9feb95aa4fd5a8f897592bfd9ffdb25a7e7fe
Anthony Delage <anthony.delage@gmail.com>
2015-04-08 12:30:42 -0400

    Changed task stack size.

--- ucosMon/Monitor.c
+++ ucosMon/Monitor.c
@@ -31,17 +31,6 @@
 #include "system.h"
 #include "gp.h"
 
-#include "AirbagModel.h"
-#include "CollisionAvoidance.h"
-#include "CruiseControlSystem.h"
-#include "FuelSensor.h"
-#include "TractionControl.h"
-#include "TransmissionControl.h"
-#include "Gain.h"
-#include "Sum.h"
-#include "Derivative.h"
-#include "Compare.h"
-
 /***********************************
  * Local Variables
  **********************************/
@@ -62,7 +51,7 @@ int *isr_1_ptr = (int *) PROCESSOR1_0_CPU_IRQ_0_BASE;
  *
  * TASK STACK DECLARATIONS
  */
-#define   TASK_STACKSIZE       768						//Stack size for all tasks
+#define   TASK_STACKSIZE       1000						//Stack size for all tasks. Originally 768.
 OS_STK schedule_task_stk[TASK_STACKSIZE] __attribute__ ((section (".critical")));
 OS_STK print_status_stk[TASK_STACKSIZE] __attribute__ ((section (".critical")));
 OS_STK print_execution_times_stk[TASK_STACKSIZE] __attribute__ ((section (".critical")));
