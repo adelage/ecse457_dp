3442a2e77a59ffb1d2900a81b4f7d46a7fd4ec9f
Anthony Delage <anthony.delage@gmail.com>
2015-04-08 15:16:06 -0400

    Added + OS_TIME_GET_SET_EN to initialization of tasks in monitor.

--- ucosMon/Monitor.c
+++ ucosMon/Monitor.c
@@ -463,10 +463,11 @@ int main(void) {
 	int arg_5 = CRITICAL_TASK_PRIORITY;
 	OSTaskCreateExt(schedule_task, &arg_5, &schedule_task_stk[TASK_STACKSIZE - 1],
 			SCHEDULE_TASK_PRIORITY, SCHEDULE_TASK_PRIORITY,
-			schedule_task_stk, TASK_STACKSIZE, NULL,OS_TASK_OPT_STK_CHK + OS_TASK_OPT_STK_CLR);
+			schedule_task_stk, TASK_STACKSIZE, NULL, OS_TASK_OPT_STK_CHK + OS_TASK_OPT_STK_CLR + OS_TIME_GET_SET_EN);
+	
 	OSTaskCreateExt(print_status_task, &arg_5, &print_status_stk[TASK_STACKSIZE - 1],
-				PRINT_STATUS_PRIORITY, PRINT_STATUS_PRIORITY,
-				print_status_stk, TASK_STACKSIZE, NULL,OS_TASK_OPT_STK_CHK + OS_TASK_OPT_STK_CLR);
+			PRINT_STATUS_PRIORITY, PRINT_STATUS_PRIORITY,
+			print_status_stk, TASK_STACKSIZE, NULL,OS_TASK_OPT_STK_CHK + OS_TASK_OPT_STK_CLR + OS_TIME_GET_SET_EN);
 
 	//Start operating system
 	OSStart();
